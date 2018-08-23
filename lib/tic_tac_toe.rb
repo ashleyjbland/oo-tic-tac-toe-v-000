b0b4b2489af4ca7e2b4b201934718f095d554d85
Ashley Bland <ashleyjbland@gmail.com>
2018-08-23 16:25:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,9 +79,8 @@ def won?(board)
 
         (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
           (position_1 == "O" && position_2 == "O" && position_3 == "O")
-
-
-  end
+        end
+      end
 end
 
   def full?(board)
